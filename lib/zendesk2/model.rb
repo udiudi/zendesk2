42569c9a68f63c24848fb9f817c8dcc9fc632a37
Josh Lane <me@joshualane.com>
2015-11-10 09:30:53 -0800

    peel back unnecessary Client namespace

---
+++ lib/zendesk2/model.rb
@@ -0,0 +1,40 @@
+class Zendesk2::Model
+  include Cistern::Model
+
+  attr_accessor :errors
+
+  # @abstract override in subclass
+  # @raise [Zendesk2::Error] if unsuccessful
+  def save!
+    raise NotImplementedError
+  end
+
+  # calls {#save!} and sets {#errors} if unsuccessful and applicable
+  # @return [Zendesk2::Model] self, regardless of success
+  def save
+    save!
+  rescue Zendesk2::Error => exception
+    self.errors = exception.response[:body]["details"].inject({}){|r,(k,v)| r.merge(k => v.map{|e| e["type"] || e["description"]})} rescue nil
+    self
+  end
+
+  def destroyed?
+    !self.reload
+  end
+
+  def destroy
+    destroy!
+  rescue Zendesk2::Error
+    false
+  end
+
+  # re-define Cistern::Attributes#missing_attributes to require non-blank
+  def missing_attributes(args)
+    ([:service] | args).select{|arg| val = send("#{arg}"); val.nil? || val == "" }
+  end
+
+  def update!(attributes)
+    merge_attributes(attributes)
+    save!
+  end
+end
---
+++ lib/zendesk2/requests/get_help_center_sections_articles.rb
@@ -0,0 +1,19 @@
+class Zendesk2::GetHelpCenterSectionsArticles < Zendesk2::Request
+  request_path { |r|
+    "/help_center/sections/#{r.section_id}/articles.json"
+  }
+
+  page_params!
+
+  def section_id
+    Integer(
+      params.fetch("section_id")
+    )
+  end
+
+  def mock
+    self.find!(:help_center_sections, section_id)
+
+    mock_response("articles" => self.data[:help_center_articles].values.select { |s| s["section_id"] == section_id })
+  end
+end
